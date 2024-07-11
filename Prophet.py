8d3a0146c4738d8b6f86355c138123cb45726bdc
jamie082 <jamiemorrissey02@gmail.com>
2024-07-11 02:12:41 -0700

    added files

--- Prophet.py
+++
@@ -1,9 +0,0 @@
-from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly
-data = pd.read_csv("DailyDelhiClimateTrain.csv")
-model = Prophet()
-forecast_data = data.rename(columns = {"date": "ds", "meantemp": "y"})
-model.fit(forecast_data)
-forecasts = model.make_future_dataframe(periods=365)
-predictions = model.predict(forecasts)
-plot_plotly(model, predictions)
--- weather.py
+++ weather.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-from prophet import Prophet
 import numpy as np
+from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
-model = Prophet()
 data = pd.read_csv("DailyDelhiClimateTrain.csv")
-forecast_data = data.rename(columns = {"date": "ds", "meantemp": "y"})
+forecast_data = data.rename(columns = {"date": "ds", 
+                                       "meantemp": "y"})
+model = Prophet()
 model.fit(forecast_data)
 forecasts = model.make_future_dataframe(periods=365)
 predictions = model.predict(forecasts)
