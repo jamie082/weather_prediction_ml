1a1791cb96ad5e946b6b0d4f4b166562cb2b32ac
jamie082 <jamiemorrissey02@gmail.com>
2024-07-11 00:59:40 -0700

    added support for Meta Prophet

--- Prophet.py
+++ Prophet.py
@@ -1,13 +1,7 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
 from prophet import Prophet
-import numpy as np
 from prophet.plot import plot_plotly, plot_components_plotly
-model = Prophet()
 data = pd.read_csv("DailyDelhiClimateTrain.csv")
+model = Prophet()
 forecast_data = data.rename(columns = {"date": "ds", "meantemp": "y"})
 model.fit(forecast_data)
 forecasts = model.make_future_dataframe(periods=365)
